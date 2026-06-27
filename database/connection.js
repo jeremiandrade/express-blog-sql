13595eb581720bf5871b804dad50107944cfceb7
jeremiandrade <jeremiandrade04@gmail.com>
2026-06-27 12:30:15 +0200

    fix: correct database credentials and enhance query error handling

--- database/connection.js
+++ database/connection.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2')
 
 const credentials = {
-    host: process.env = DB_HOST,
-    user: process.env = DB_USER,
-    password: process.env = DB_PASSWORD,
-    name: process.env = DB_NAME
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 }
 
 
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000;
 
+//importo il file di connessione al database
+const connection = require('./database/connection')
+
 
 //start server 
 app.listen(port, () => {
@@ -13,5 +16,60 @@ app.listen(port, () => {
 app.get('/', (req, res) => {
     res.json({ message: 'welcome to express SQL' })
 })
+
+
+//INDEX
+const index = (req, res) => {
+    //preparo la query
+    const sql = ' SELECT * FROM POSTS'
+    //eseguo la query
+    connection.query(sql, (err, results) => {
+        console.log(err);
+        if (err) {
+            console.error('Error executing query:', err);
+            return res.status(500).json({ error: true, message: 'Internal Server Error' })
+        }
+        console.log(results);
+        res.json(results)
+    })
+}
+
+
+// route post
+app.get('/posts', index)
+
+
+//SHOW
+
+const show = (req, res) => {
+    //recupero id dall'url
+    const id = req.params.id
+    const sql = ' SELECT * FROM POSTS WHERE id =?'
+    connection.query(sql, [id], (err, results) => {
+        if (err) return res.status(500).json({ error: 'Database query failed' })
+        if (results.lenght === 0) return req.status(400).json({ error: 'Post not found' })
+        res.json(results[0])
+    })
+}
+
+app.get('/posts/:id', show);
+
+
+// Facciamo sì che l’API di DESTROY permetta di eliminare un post dal database
+// Verifichiamo su Postman che la chiamata non dia errore e risponda 204
+// Verifichiamo su MySQL Workbench che il post venga effettivamente rimosso
+
+
+//DESTROY
+
+const destroy = (req, res) => {
+
+    //recupero l'id 
+    const { id } = req.params
+    // elimino elemento 
+
+
+}
+
 
 
